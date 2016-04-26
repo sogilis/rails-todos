72409ea569e57205eec9bdcd918aeb82eeca9755
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2016-04-26 16:41:22 +0200

    Add todos#create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/todos", to: "todos#index"
   get "/todos/new", to: "todos#new"
+  post "/todos", to: "todos#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
