e887e9f80b354e21e5cc40ddd528ea77b77e738a
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2016-05-01 19:24:27 +0200

    resources :todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get "/todos", to: "todos#index"
-  get "/todos/new", to: "todos#new", as: :new_todo
-  post "/todos", to: "todos#create"
-  get "/todos/:id", to: "todos#show", as: :todo
-  get "/todos/:id/edit", to: "todos#edit", as: :edit_todo
-  patch "/todos/:id", to: "todos#update"
-  delete "/todos/:id", to: "todos#destroy"
+  resources :todos
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
